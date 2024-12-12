6ab1799e1b75f268a1e14549e7ba18c4e5bf2e68
Zachary Pitcher <zackpi@mit.edu>
2024-12-12 14:22:06 -0800

    day12

--- day12/index.js
+++ day12/index.js
@@ -1,4 +1,4 @@
-let raw = await Bun.file("day12/input_small2").text();
+let raw = await Bun.file("day12/input").text();
 
 let cells = raw.split("\n").map((l) => l.split(""));
 let h = cells.length;
@@ -7,9 +7,7 @@ let w = cells[0].length;
 let k = ([x, y]) => x + "_" + y;
 let seen = new Set();
 
-let boolgrid = (x, y) =>
-  Array.from({ length: y }).map(() => Array(x).fill(false));
-let toggle = (a, x, y) => (a[y][x] = !a[y][x]);
+let grid = () => Array.from({ length: h + 1 }).map(() => Array(w + 1).fill(0));
 
 function buildClique(r) {
   let letter = cells[r[1]][r[0]];
@@ -17,18 +15,18 @@ function buildClique(r) {
   let nodes = [r];
   seen.add(k(r));
 
-  let verts = boolgrid(w + 1, h);
-  let horiz = boolgrid(w, h + 1);
+  let verts = grid();
+  let horiz = grid();
 
   while (nodes.length) {
     let nextNodes = [];
     for (let n of nodes) {
       let [x, y] = n;
 
-      toggle(verts, x, y);
-      toggle(verts, x + 1, y);
-      toggle(horiz, x, y);
-      toggle(horiz, x, y + 1);
+      verts[y][x] += 1;
+      verts[y][x + 1] -= 1;
+      horiz[y][x] += 1;
+      horiz[y + 1][x] -= 1;
 
       for (let adj of [
         [x, y - 1],
@@ -50,94 +48,50 @@ function buildClique(r) {
   }
 
   let perimeter = 0;
-  let uniques = 0;
+  let nHoriz = 0;
   for (let j = 0; j < h + 1; j++) {
-    let onedge = false;
+    let onedge = 0;
     for (let i = 0; i < w; i++) {
-      if (horiz[j][i]) {
+      let h = horiz[j][i];
+      if (h !== 0) {
         perimeter++;
-        if (!onedge) uniques++;
-        onedge = true;
-      } else {
-        onedge = false;
+        if (h !== onedge) nHoriz++;
       }
+      onedge = h;
     }
   }
+  let nVerts = 0;
   for (let i = 0; i < w + 1; i++) {
-    let onedge = false;
+    let onedge = 0;
     for (let j = 0; j < h; j++) {
-      if (verts[j][i]) {
+      let v = verts[j][i];
+      if (v !== 0) {
         perimeter++;
-        if (!onedge) uniques++;
-        onedge = true;
-      } else {
-        onedge = false;
+        if (v !== onedge) nVerts++;
       }
+      onedge = v;
     }
   }
+  let uniques = nVerts + nHoriz;
 
-  return [letter, clique.length, perimeter, uniques];
+  return [clique.length, perimeter, uniques];
 }
 
-let sum = 0;
-let sum2 = 0;
+let part1 = 0;
+let part2 = 0;
 for (let j = 0; j < h; j++) {
   for (let i = 0; i < w; i++) {
     let root = [i, j];
     if (seen.has(k(root))) continue;
 
-    let [letter, area, perimeter, uniques] = buildClique(root);
-    // process.exit();
-    sum += area * perimeter;
-    sum2 += area * uniques;
+    let [area, perimeter, uniques] = buildClique(root);
+    part1 += area * perimeter;
+    part2 += area * uniques;
   }
 }
 
-let part1 = sum;
 console.log("part1 =", part1);
 // =1473408
 
-let part2 = sum2;
 console.log("part2 =", part2);
-// =
-
-// let edges = {};
-// for (let n of clique) {
-//   // for (let key of [
-//   //   k(n) + "_down",
-//   //   k(n) + "_right",
-//   //   k([n[0] + 1, n[1]]) + "_down",
-//   //   k([n[0], n[1] + 1]) + "_right",
-//   // ]) {
-//   //   if (key in edges) edges[key] = false;
-//   //   else edges[key] = true;
-//   // }
-
-//   // vert
-//   for (let ox of [0, 1]) {
-//     let x = n[0] + ox;
-//     let sy = n[1];
-//     let ey = sy + 1;
-
-//   for (let [ox, oy, dx, dy, m] of [
-//     [[0, 0, 0, 1, 1]],
-//     [[0, 0, 1, 0, 0]],
-//     [[1, 0, 0, 1, 1]],
-//     [[0, 1, 1, 0, 0]],
-//   ]) {
-//     let sx = n[0] + ox;
-//     let sy = n[1] + oy;
-//     let ex = sx + dx;
-//     let ey = sy + dy;
-
-//     // try to find edge above or below to connect to
-//     let uniqueEdge = uniqueEdges.find((edge) => {
-//       if (edge[4] === m) {
-//         if (edge[0] === ex && edge[1] === ey) {
-//         }
-//       }
-//     });
-//   }
-// }
-
-// let perimeter = Object.values(edges).filter((v) => !!v).length;
+// =886364
