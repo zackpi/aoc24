a26d905f098da8db991c01d020fb28192cc21a94
Zachary Pitcher <zackpi@mit.edu>
2024-12-21 09:41:20 -0800

    day21p1

--- day21/index.js
+++ day21/index.js
@@ -1,4 +1,4 @@
-let raw = await Bun.file("day21/input_small").text();
+let raw = await Bun.file("day21/input").text();
 let codes = raw.split("\n");
 
 /*
@@ -25,60 +25,72 @@ let pair = (i) => [i % 3, (i / 3) | 0];
 let numpad = "789456123_0A";
 let dpad = "_^A<v>";
 
-// function permute(dx, dy) {
-//   let h = dx < 0 ? "<" : ">";
-//   let v = dy < 0 ? "^" : "v";
-//   let ax = Math.abs(dx);
-//   let ay = Math.abs(dy);
-
-//   let total = ax + ay;
-//   let perms = [];
-//   for (let i = 0; i < 1 << total; i++) {
-//     let perm = i
-//       .toString(2)
-//       .padStart(total, "0")
-//       .replace(/./g, (x) => (x === "0" ? h : v));
-//     perms.push(perm);
-//   }
-
-//   return perms;
-// }
-
 let ch = {};
-function choose(dx, dy) {
-  let key = `${dx},${dy}`;
+function choose(pad, x, y, dx, dy) {
+  if (pad[x + y * 3] === "_") return [];
+
+  let key = `${pad}|${x},${y}|${dx},${dy}`;
   if (key in ch) return ch[key];
 
   let h = dx < 0 ? "<" : ">";
   let v = dy < 0 ? "^" : "v";
-  if (dx === 0) return [v.repeat(dy)];
-  if (dy === 0) return [h.repeat(dx)];
   let sx = Math.sign(dx);
   let sy = Math.sign(dy);
-  let hs = choose(dx - sx, dy).map((x) => h + x);
-  let vs = choose(dx, dy - sy).map((x) => v + x);
+  if (sx === 0) return [v.repeat(sy * dy)];
+  if (sy === 0) return [h.repeat(sx * dx)];
+
+  let hs = choose(pad, x + sx, y, dx - sx, dy).map((x) => h + x);
+  let vs = choose(pad, x, y + sy, dx, dy - sy).map((x) => v + x);
 
   let ret = hs.concat(vs);
   ch[key] = ret;
   return ret;
 }
 
-process.exit(0);
-
-function complexity(bots, code) {
-  let [pad] = bots[0];
-
-  let seqs = [];
+function sequence(pad, code) {
+  let seqs = [[]];
+  let last = "A";
   for (let char of code) {
-  }
+    let [sx, sy] = pair(pad.indexOf(last));
+    let [ex, ey] = pair(pad.indexOf(char));
+    let dx = ex - sx;
+    let dy = ey - sy;
 
-  complexity(bots.slice(1));
+    let newSeqs = [];
+    for (let seq of seqs) {
+      let paths = choose(pad, sx, sy, dx, dy);
+      for (let path of paths) {
+        newSeqs.push(seq.concat(path));
+      }
+    }
+
+    seqs = newSeqs;
+    last = char;
+  }
+  return seqs.map((s) => s.join("A") + "A");
 }
 
-let bots = [
-  [numpad, ["A"]],
-  [dpad, ["A"]],
-  [dpad, ["A"]],
-  // one more dpad, controlled by user
-];
-console.log(complexity(bots, "029A"));
+function complexity(code) {
+  let minComplexity = Infinity;
+  let minSequence = null;
+  for (let seqNum of sequence(numpad, code)) {
+    for (let seqDir1 of sequence(dpad, seqNum)) {
+      for (let seq of sequence(dpad, seqDir1)) {
+        let complexity = seq.length;
+        if (complexity < minComplexity) {
+          minComplexity = complexity;
+          minSequence = seq;
+        }
+      }
+    }
+  }
+  return minComplexity;
+}
+
+let sum = 0;
+for (let code of codes) {
+  let cmp = complexity(code);
+  let num = Number(code.slice(0, 3));
+  sum += cmp * num;
+}
+console.log(sum);
