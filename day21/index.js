1e4f135e13cdf5504c71b012b3d1af06451367b0
Zachary Pitcher <zackpi@mit.edu>
2024-12-21 08:28:31 -0800

    choose path

--- day21/index.js
+++ day21/index.js
@@ -1,6 +1,5 @@
 let raw = await Bun.file("day21/input_small").text();
 let codes = raw.split("\n");
-let pair = (i) => [i % 3, (i / 3) | 0];
 
 /*
 dpad:
@@ -9,25 +8,7 @@ dpad:
 +---+---+---+
 | < | v | > |
 +---+---+---+
-*/
-function dpad(start, end) {
-  let keys = "_^A<v>";
-  let h = 2;
-  let s = keys.indexOf(start);
-  let e = keys.indexOf(end);
-  let [sx, sy] = pair(s);
-  let [ex, ey] = pair(e);
-  let dx = ex - sx;
-  let dy = ey - sy;
-  let result = "";
-  if (dy > 0) result += "v".repeat(dy);
-  if (dx < 0) result += "<".repeat(-dx);
-  if (dx > 0) result += ">".repeat(dx);
-  if (dy < 0) result += "^".repeat(-dy);
-  return result + "A";
-}
 
-/*
 numpad:
 +---+---+---+
 | 7 | 8 | 9 |
@@ -39,69 +20,65 @@ numpad:
     | 0 | A |
     +---+---+
 */
-function numpad(start, end) {
-  let keys = "789456123_0A";
-  let s = keys.indexOf(start);
-  let e = keys.indexOf(end);
-  let h = 4;
-  let [sx, sy] = pair(s);
-  let [ex, ey] = pair(e);
 
-  let dx = ex - sx;
-  let dy = ey - sy;
-  let seq = "";
-  if (dy < 0) seq += "^".repeat(-dy);
-  if (dx < 0) seq += "<".repeat(-dx);
-  if (dx > 0) seq += ">".repeat(dx);
-  if (dy > 0) seq += "v".repeat(dy);
-  seq += "A";
+let pair = (i) => [i % 3, (i / 3) | 0];
+let numpad = "789456123_0A";
+let dpad = "_^A<v>";
 
-  return seq;
+// function permute(dx, dy) {
+//   let h = dx < 0 ? "<" : ">";
+//   let v = dy < 0 ? "^" : "v";
+//   let ax = Math.abs(dx);
+//   let ay = Math.abs(dy);
+
+//   let total = ax + ay;
+//   let perms = [];
+//   for (let i = 0; i < 1 << total; i++) {
+//     let perm = i
+//       .toString(2)
+//       .padStart(total, "0")
+//       .replace(/./g, (x) => (x === "0" ? h : v));
+//     perms.push(perm);
+//   }
+
+//   return perms;
+// }
+
+let ch = {};
+function choose(dx, dy) {
+  let key = `${dx},${dy}`;
+  if (key in ch) return ch[key];
+
+  let h = dx < 0 ? "<" : ">";
+  let v = dy < 0 ? "^" : "v";
+  if (dx === 0) return [v.repeat(dy)];
+  if (dy === 0) return [h.repeat(dx)];
+  let sx = Math.sign(dx);
+  let sy = Math.sign(dy);
+  let hs = choose(dx - sx, dy).map((x) => h + x);
+  let vs = choose(dx, dy - sy).map((x) => v + x);
+
+  let ret = hs.concat(vs);
+  ch[key] = ret;
+  return ret;
 }
 
-let press = (code, pad) => {
-  let seq = "";
-  let last = "A";
+process.exit(0);
+
+function complexity(bots, code) {
+  let [pad] = bots[0];
+
+  let seqs = [];
   for (let char of code) {
-    seq += pad(last, char);
-    last = char;
   }
-  return seq;
-};
 
-let myComplexity = 0;
-for (let code of codes) {
-  let codeNum = press(code, numpad);
-  let codeDir = press(codeNum, dpad);
-  let codeSelf = press(codeDir, dpad);
-  console.log(code);
-  console.log(codeNum);
-  console.log(codeDir);
-  console.log(codeSelf);
-  console.log();
-
-  myComplexity += codeSelf.length * Number(code.slice(0, 3));
+  complexity(bots.slice(1));
 }
-console.log("myComplexity =", myComplexity);
 
-let tests = [
-  "029A: <vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A",
-  "980A: <v<A>>^AAAvA^A<vA<AA>>^AvAA<^A>A<v<A>A>^AAAvA<^A>A<vA>^A<A>A",
-  "179A: <v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A",
-  "456A: <v<A>>^AA<vA<A>>^AAvAA<^A>A<vA>^A<A>A<vA>^A<A>A<v<A>A>^AAvA<^A>A",
-  "379A: <v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A",
-].map((x) => x.split(": "));
-
-let complexity = 0;
-for (let [code, seq] of tests) {
-  complexity += seq.length * Number(code.slice(0, 3));
-}
-console.log("complexity =", complexity);
-
-// let part1 = 0;
-// console.log("part1 =", part1);
-// =
-
-// let part2 = 0;
-// console.log("part2 =", part2);
-// =
+let bots = [
+  [numpad, ["A"]],
+  [dpad, ["A"]],
+  [dpad, ["A"]],
+  // one more dpad, controlled by user
+];
+console.log(complexity(bots, "029A"));
